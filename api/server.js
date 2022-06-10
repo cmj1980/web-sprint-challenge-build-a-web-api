17a2dcf986897a51be096adf954376986e329b62
cmj1980 <dagoclmt@gmail.com>
2022-06-09 22:25:46 -0700

    initial commit server, index, .env configured

--- api/server.js
+++ api/server.js
@@ -6,4 +6,8 @@ const server = express();
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+server.get('/', (req, res) => {
+    res.send(`<h1>Welcome To Chuck 1st API!</h1>`)
+});
+
 module.exports = server;
