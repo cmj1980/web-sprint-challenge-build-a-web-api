789388a7882d44f8aaa0cc61aa3a971d9744115c
cmj1980 <dagoclmt@gmail.com>
2022-06-10 06:49:14 -0700

    server, prjt-rtr, & prjt-mid plugged in and working server up

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,22 @@
 // Write your "projects" router here!
+const Projects = require('./projects-model');
+const express = require('express');
+
+const router = express.Router();
+const { validateProjectId } = require('./projects-middleware');
+
+router.get('/', (req, res) => {
+    Projects.get()
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({ 
+            message: 'The posts information could not be retrieved'
+         })
+    })
+})
+
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
+
+
+const projectsRouter = require('./projects/projects-router');
+server.use('/api/projects', projectsRouter);
+// const actionsRouter = require('./actions/actions-router');
+// server.use('/api/actions', actionsRouter)
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
