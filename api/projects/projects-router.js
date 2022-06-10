89cd2590492776560a95c7b5e3ee218898f56ac9
cmj1980 <dagoclmt@gmail.com>
2022-06-10 10:58:27 -0700

    projects post, put, delete added

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,19 +3,71 @@ const Projects = require('./projects-model');
 const express = require('express');
 
 const router = express.Router();
-const { validateProjectId } = require('./projects-middleware');
+const {
+    validateProjectId,
+    validateProject
+} = require('./projects-middleware');
 
 router.get('/', (req, res) => {
     Projects.get()
-    .then(project => {
-        res.status(200).json(project)
-    })
-    .catch(err => {
-        res.status(500).json({ 
-            message: 'The posts information could not be retrieved'
-         })
-    })
-})
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'The posts information could not be retrieved'
+            })
+        })
+});
+
+router.get('/:id', validateProjectId, async (req, res, next) => {
+    try {
+        res.status(200).json()
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.post('/', (req, res) => {
+    const newProject = req.body
+    Projects.insert(newProject)
+        .then(project => {
+            res.status(200).json(newProject)
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'Error adding new project'
+            })
+        })
+});
+
+router.put('/:id', validateProjectId, validateProject, async (req, res, next) => {
+    if (req.body === undefined) {
+        next({
+            status: 400,
+            message: 'Missing required fields'
+        })
+    } else {
+        try {
+            const updatedProject = await Projects.update(req.params.id, req.body)
+            res.status(200).json(updatedProject)
+        } catch (err) {
+            next(err)
+        }
+    }
+});
+
+router.delete('/:id', validateProjectId, async (req, res, next) => {
+    try {
+        await Projects.remove(req.params.id)
+        res.json(res.Projects)
+    } catch(err){
+        next(err)
+    }
+});
+
+
+
 
 
 
